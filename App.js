911ca7123e1dcd8679ccce127c66f6ed91dd62a8
Charandeep Singh <114646455+Charandeepoo7@users.noreply.github.com>
2023-02-15 20:54:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+import {Test1} from './Component/Test1'
+function App() {
+  return (
+    <div className="App">
+      <Test1 />
+    </div>
+  );
+}
+
+export default App;
